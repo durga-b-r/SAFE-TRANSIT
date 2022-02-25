b1bcf8fcb5975014e30fe1973cc22ea33b7f3202
DURGA B R <62275615+durga-b-r@users.noreply.github.com>
2022-02-25 23:38:47 +0530

    Add files via upload

---
+++ SAFE_TRANSIT_COMMU.ino
@@ -0,0 +1,72 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti WiFiMulti;
+
+const char* ssid = "ESP8266-Access-Point";
+const char* password = "123456789";
+
+#include <Wire.h>
+unsigned long previousMillis = 0;
+const long interval = 5000; 
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Setup Complete");
+ 
+  Serial.print("Connecting to ");
+  
+  Serial.println(ssid);
+  Serial.println("Initialising wifi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  
+  if(currentMillis - previousMillis >= interval) {
+     // Check WiFi connection status
+    if ((WiFiMulti.run() == WL_CONNECTED)) {      
+      // save the last HTTP GET Request
+      previousMillis = currentMillis;
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+  }
+}
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your IP address with path or Domain name with URL path 
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "--"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
